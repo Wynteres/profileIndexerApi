ce7cc72cdbd6a6f810f0702b66c0a65a982ab98b
Raul Coan <rvc.coan@gmail.com>
2019-05-01 12:48:55 -0300

    profile validations and controller methods added

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -1,17 +1,59 @@
 class Api::V1::ProfileController < ApplicationController
   def index
-    return render :json => {'Teste':'teste'}
+    #fetch all profiles
+    return render :json => {:profiles => Profile::all()}
   end
 
   def show
+    #fetch profile
+    return render :json => {:profile => Profile::find(params[:id])}
   end
 
   def create
+    #creates a new profile object to validation and persistence
+    profile = Profile.new(name: params[:profile][:name], twitterUrl: params[:profile][:twitterUrl])
+
+    #verify if there's any error at profile fields and return if they exists
+    return render :json => {:errors => profile.errors}, :status => :bad_request if not profile.valid?
+    
+
+    #persist the profile at database
+    profile.save
+
+    #return the persisted profile
+    return render :json => {:profile => profile}, :status => :created
+
   end
 
   def update
+    #Gets existing profile object to update, validate and persist data
+    profile = Profile::find(params[:id])
+
+    #update fetched profile data 
+    profile.name = params[:profile][:name]
+    profile.twitterUrl = params[:profile][:twitterUrl]
+    profile.twitterUsername = params[:profile][:twitterUsername]
+    profile.twitterDescription = params[:profile][:twitterDescription]
+
+    #verify if there's any error at profile fields and return if they exists
+    return render :json => {:errors => profile.errors}, :status => :bad_request if not profile.valid?
+    
+    #persist the profile at database
+    profile.save
+
+    #return the persisted profile
+    return render :json => {:profile => profile}, :status => :success
   end
 
   def destroy
+
+    #Get  exiting profile to delete
+    profile = Profile::find(params[:id])
+
+    #delete the profile
+    profile.destroy
+
+    #return status with no content
+    return render :json => '', :status => :no_content
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,24 @@
 class Profile < ActiveRecord::Base
+    #creates validations for every field for every data manipulation action
+    validates :name,
+        :presence => {:message => "O nome deve estar preenchido"},
+        :format => {:with => /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/, :multiline => true, :message => "O nome deve conter apenas letras"},
+        :length => { :minimum => 2, :maximum => 255, 
+            :too_short => "O nome deve ter pelo menos %{count} caracteres", 
+            :too_long => "O nome deve ter no maximo %{count} caracteres" }
+    
+    validates :twitterUrl, 
+        :presence => {:message => "A URL do perfil do twitter deve estar preenchida"},
+        :format => { :with => /(?:http(?:s)?:\/\/)?(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/, 
+            :message => "A URL deve ser uma url válida do twitter" },
+            :length => { :maximum => 255, :too_long => "O nome deve ter no maximo %{count} caracteres" }
+    
+    validates :twitterUsername, 
+        :allow_blank => true,
+        :length => { :maximum => 15, :too_long => "Seu nome do twitter deve ter no máximo %{count} caracteres" }
+            
+    validates :twitterDescription, 
+        :allow_blank => true,
+        :length => { :maximum => 15, :too_long => "Sua descrição do twitter deve ter no máximo %{count} caracteres" }
+
 end
